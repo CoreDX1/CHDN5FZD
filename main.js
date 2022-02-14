9f7cc05df0d7515b240901e984619387799ce0b9
CoreDX1 <chrismaquis@gmail.com>
2022-02-14 11:36:13 -0300

    Funciona el Prompt y Array ||02-14 || 11:12

--- main.js
+++ main.js
@@ -1,7 +1,11 @@
+let nombre = prompt("Ingrese su Nombre");
+const cuadro = document.getElementById("cuadro");
+let nombreIngresado = document.createElement("h1");
+nombreIngresado.innerHTML = `${nombre}`;
+cuadro.prepend(nombreIngresado);
+
 let menus = ["Menu", "Explorar", "Carrito", "¿Quines Somos?"]
-
 const menu_Primario = document.getElementById("menu_Primario")
-
 for (const menuPagina of menus){
     let contenedor = document.createElement("li")
     contenedor.className = "menuPagina";
